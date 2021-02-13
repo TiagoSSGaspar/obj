ccbf10f068c8d9145b0bf645f17c7f0dd89146d9
Tiago S S Gaspar <tiago.sousasilvagaspar@gmail.com>
2021-02-13 18:36:33 -0300

    H2 database, test profile, JPA

--- src/main/java/com/objpedidoweb/obj/entities/User.java
+++ src/main/java/com/objpedidoweb/obj/entities/User.java
@@ -2,22 +2,38 @@ package com.objpedidoweb.obj.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-public @Data class User implements Serializable {
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private long id;
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Getter long id;
 	
+	private @Getter @Setter String name;
+	
+	private @Getter @Setter String email;
+	
+	private @Getter @Setter String phone;
+	
+	private @Getter @Setter String password;
 	
 
 }
