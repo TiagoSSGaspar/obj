d71864c5c49427d0c73178868a11d212a71b5e65
Tiago S S Gaspar <tiago.sousasilvagaspar@gmail.com>
2021-02-13 11:56:36 -0300

    Project created

---
+++ src/main/java/com/objpedidoweb/obj/ObjApplication.java
@@ -0,0 +1,13 @@
+package com.objpedidoweb.obj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/objpedidoweb/obj/ObjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objpedidoweb.obj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
