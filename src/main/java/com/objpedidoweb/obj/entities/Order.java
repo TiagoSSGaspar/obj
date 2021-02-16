cf93a081fba69cf75e9aa4e59acbda03b70d547e
Tiago S S Gaspar <tiago.sousasilvagaspar@gmail.com>
2021-02-16 11:49:01 -0300

    OrderStatus enum

--- src/main/java/com/objpedidoweb/obj/entities/Order.java
+++ src/main/java/com/objpedidoweb/obj/entities/Order.java
@@ -12,13 +12,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.objpedidoweb.obj.entities.enums.OrderStatus;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tb_order")
 public @Data class Order implements Serializable {
@@ -31,8 +30,39 @@ public @Data class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-
+	
+	
+	private Integer orderStatus; 
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/objpedidoweb/obj/entities/User.java
+++ src/main/java/com/objpedidoweb/obj/entities/User.java
@@ -13,9 +13,11 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @NoArgsConstructor
@@ -23,34 +25,27 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tb_user")
 public @Data class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
+	@Setter(value = AccessLevel.NONE) private long id;
+
 	private String name;
-	
+
 	private String email;
-	
+
 	private String phone;
-	
+
 	private String password;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+	
 
-	public User(String name, String email, String phone, String password) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
-	}
-	
-	
-	
 
 }
